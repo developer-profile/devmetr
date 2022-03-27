f7594a525718e22440897cb64e43b6f21090134a
Dimitri Molokov <d@molokov.de>
2022-03-27 13:26:07 +0300

    Agent service for increment1

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,40 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	GetMetrics(2)
+
+}
+
+func GetMetrics(duration time.Duration) {
+	var interval = duration * time.Second
+	s := 0  // steps counter
+	cs := 0 // send to server counter
+	i := 0  // for increment base value
+	for {
+		<-time.After(interval)
+
+		s += 1
+		if i < 5 {
+			i += 1
+			//s := fmt.Sprintf("%s is %d years old.\n", name, age)
+			fmt.Printf("Step #%d collecting data with 2 seconds interval %d \n", s, i)
+		} else {
+			i = 1
+			cs += 1
+			fmt.Printf("Step #%d sending data to 127.0.0.1:8080/update: #%d \n", s, cs)
+			fmt.Printf("Step #%d collecting data with 2 seconds interval %d \n", s, i)
+
+		}
+		if s > 5 {
+			fmt.Printf("Total steps: %d \nTotal server update: %d \nExiting.. \n", s, cs)
+			return
+		}
+
+	}
+}
