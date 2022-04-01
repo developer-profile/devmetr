8672425cee22083bcd33e7460330a92288333b82
Dimitri Molokov <d@molokov.de>
2022-04-02 00:10:29 +0300

    Agent and server middleware.go and handlers.go and mod.go for service update for increment2

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,120 +1,14 @@
 package handlers
 
 import (
-	"encoding/json"
 	"fmt"
-	"github.com/developer-profile/devmetr.git/internal/login"
 	"log"
 	"net/http"
 	"net/http/httputil"
-	"os"
 	"strings"
 )
 
-func StatusHandler1(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	// намеренно сделана ошибка в JSON
-	_, err := w.Write([]byte(`{"status":"ok"}`))
-	if err != nil {
-		log.Panicln(err)
-		os.Exit(1)
-	}
-
-}
-
-func StatusHandler(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		// намеренно сделана ошибка в JSON
-		_, err := w.Write([]byte(`{"status":"ok"}`))
-		if err != nil {
-			log.Panicln(err)
-			os.Exit(1)
-		}
-		next.ServeHTTP(w, r)
-	})
-}
-
-func LoginHandler1(w http.ResponseWriter, r *http.Request) {
-	login.Login(w, r)
-
-}
-
-func LoginHandler(next http.Handler) http.Handler {
-	return http.Handler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		login.Login(w, r)
-		next.ServeHTTP(w, r)
-	}),
-	)
-}
-
-func UpdateHandler1(w http.ResponseWriter, r *http.Request) {
-	// сначала устанавливаем заголовок Content-Type
-	// для передачи клиенту информации, кодированной в JSON
-	w.Header().Set("content-type", "application/json")
-	// устанавливаем статус-код 200
-	w.WriteHeader(http.StatusOK)
-
-	resp, err := json.Marshal(r)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
-	// If you want a good line of code to get both query or form parameters
-	// you can do the following:
-	param1 := r.FormValue("Sys")
-	fmt.Fprintf(w, "Parameter1:  %s ", param1)
-
-	//to get a path parameter using the standard library simply
-	param2 := strings.Split(r.URL.Path, "/")
-
-	// make sure you handle the lack of path parameters
-	if len(param2) > 4 {
-		fmt.Fprintf(w, " Parameter2:  %s", param2[5])
-	}
-	//subj := Gauge{}
-	// пишем тело ответа
-	w.Write(resp)
-	fmt.Println(string(resp))
-
-}
-
-func UpdateHandler(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// сначала устанавливаем заголовок Content-Type
-		// для передачи клиенту информации, кодированной в JSON
-		w.Header().Set("content-type", "application/json")
-		// устанавливаем статус-код 200
-		w.WriteHeader(http.StatusOK)
-
-		resp, err := json.Marshal(r)
-		if err != nil {
-			http.Error(w, err.Error(), 500)
-			return
-		}
-		// If you want a good line of code to get both query or form parameters
-		// you can do the following:
-		param1 := r.FormValue("Sys")
-		fmt.Fprintf(w, "Parameter1:  %s ", param1)
-
-		//to get a path parameter using the standard library simply
-		param2 := strings.Split(r.URL.Path, "/")
-
-		// make sure you handle the lack of path parameters
-		if len(param2) > 4 {
-			fmt.Fprintf(w, " Parameter2:  %s", param2[5])
-		}
-		//subj := Gauge{}
-		// пишем тело ответа
-		w.Write(resp)
-		fmt.Println(string(resp))
-		next.ServeHTTP(w, r)
-	})
-}
-
-func Hello1(w http.ResponseWriter, r *http.Request) {
+func GaugeUpdate(w http.ResponseWriter, r *http.Request) {
 	requestDump, err := httputil.DumpRequest(r, true)
 	if err != nil {
 		fmt.Println(err.Error())
@@ -123,32 +17,25 @@ func Hello1(w http.ResponseWriter, r *http.Request) {
 
 	switch r.Method {
 	case "GET":
+		// сначала устанавливаем заголовок Content-Type
+		// для передачи клиенту информации, кодированной в JSON
+		w.Header().Set("content-type", "application/json")
+		// устанавливаем статус-код 200
+		w.WriteHeader(http.StatusNotAcceptable)
+
+		log.Println("GET method not acceptable")
 
 	case "POST":
-
-		//log.Println(r.RequestURI)
 		params := strings.Split(r.RequestURI, "/")
-		valueCount := true
-		valueName := ""
-		valueData := ""
-		for _, value := range params {
+		log.Printf("request parsed: \n %v \n", params)
 
-			if value != "" && value != "update" && value != "GAUGE" {
-				if valueCount {
-					valueName = value
-					valueCount = false
-				} else {
-					valueData = value
-					valueCount = true
-				}
+		switch params[1] {
+		case "gauge":
+			log.Println("Gauge")
 
-			}
-			if valueData != "" {
+		case "counter":
+			log.Println("counter")
 
-				log.Printf("%v: %v", valueName, valueData)
-				valueName = ""
-				valueData = ""
-			}
 		}
 
 	default:
@@ -156,53 +43,66 @@ func Hello1(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			log.Printf("Error %v", err)
 		}
+
+	}
+
+}
+
+func CounterUpdate(w http.ResponseWriter, r *http.Request) {
+	requestDump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(string(requestDump))
+
+	switch r.Method {
+	case "GET":
+		// сначала устанавливаем заголовок Content-Type
+		// для передачи клиенту информации, кодированной в JSON
+		w.Header().Set("content-type", "application/json")
+		// устанавливаем статус-код 200
+		w.WriteHeader(http.StatusNotAcceptable)
+
+		w.Write([]byte("Get method not acceptable"))
+
+		log.Println("GET method not acceptable")
+
+	case "POST":
+		params := strings.Split(r.RequestURI, "/")
+		log.Printf("request parsed: \n %v \n", params)
+
+		switch params[1] {
+		case "gauge":
+			log.Println("Gauge")
+
+		case "counter":
+			log.Println("counter")
+
+		}
+
+	default:
+		_, err := fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
+		if err != nil {
+			log.Printf("Error %v", err)
+		}
+
 	}
 }
 
-func Hello(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		requestDump, err := httputil.DumpRequest(r, true)
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-		fmt.Println(string(requestDump))
+func SendNotFound(w http.ResponseWriter, r *http.Request) {
+	requestDump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(string(requestDump))
 
-		switch r.Method {
-		case "GET":
+	// сначала устанавливаем заголовок Content-Type
+	// для передачи клиенту информации, кодированной в JSON
+	w.Header().Set("content-type", "text/plain")
+	// устанавливаем статус-код 200
+	w.WriteHeader(http.StatusNotFound)
 
-		case "POST":
+	w.Write([]byte("Page not fount code 404"))
 
-			//log.Println(r.RequestURI)
-			params := strings.Split(r.RequestURI, "/")
-			valueCount := true
-			valueName := ""
-			valueData := ""
-			for _, value := range params {
-
-				if value != "" && value != "update" && value != "GAUGE" {
-					if valueCount {
-						valueName = value
-						valueCount = false
-					} else {
-						valueData = value
-						valueCount = true
-					}
-
-				}
-				if valueData != "" {
-
-					log.Printf("%v: %v", valueName, valueData)
-					valueName = ""
-					valueData = ""
-				}
-			}
-
-		default:
-			_, err := fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
-			if err != nil {
-				log.Printf("Error %v", err)
-			}
-		}
-		next.ServeHTTP(w, r)
-	})
+	log.Println("Page not fount code 404")
 }
