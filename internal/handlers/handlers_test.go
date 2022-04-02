fde844ed66018a1549cbb2c77f80fd2c5b92220d
Dimitri Molokov <d@molokov.de>
2022-04-02 17:09:05 +0300

    Agent and server middleware.go and handlers.go and mod.go for service update for increment2

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -8,7 +8,7 @@ import (
 	"testing"
 )
 
-func TestStatusHandler(t *testing.T) {
+func TestSaveMetric(t *testing.T) {
 	// определяем структуру теста
 	type want struct {
 		code        int
@@ -25,20 +25,194 @@ func TestStatusHandler(t *testing.T) {
 			name: "positive test #1",
 			want: want{
 				code:        200,
-				response:    `{"status":"ok"}`,
-				contentType: "application/json",
+				response:    `Key: ALLOC => Element: 1.024456E+06`,
+				contentType: "plain/text",
 			},
 		},
 	}
 	for _, tt := range tests {
 		// запускаем каждый тест
 		t.Run(tt.name, func(t *testing.T) {
-			request := httptest.NewRequest(http.MethodGet, "/status", nil)
+			request := httptest.NewRequest(http.MethodPost, "/UPDATE/GAUGE/ALLOC/1.024456E+06/", nil)
 
 			// создаём новый Recorder
 			w := httptest.NewRecorder()
 			// определяем хендлер
-			h := http.HandlerFunc(handlers.StatusHandler)
+			h := http.HandlerFunc(handlers.SaveMetric)
+			// запускаем сервер
+			h.ServeHTTP(w, request)
+			res := w.Result()
+
+			// проверяем код ответа
+			if res.StatusCode != tt.want.code {
+				t.Errorf("Expected status code %d, got %d", tt.want.code, w.Code)
+			}
+
+			// получаем и проверяем тело запроса
+			defer res.Body.Close()
+			resBody, err := io.ReadAll(res.Body)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if string(resBody) != tt.want.response {
+				t.Errorf("Expected body %s, got %s", tt.want.response, w.Body.String())
+			}
+
+			// заголовок ответа
+			if res.Header.Get("Content-Type") != tt.want.contentType {
+				t.Errorf("Expected Content-Type %s, got %s", tt.want.contentType, res.Header.Get("Content-Type"))
+			}
+		})
+	}
+}
+
+func TestSend400(t *testing.T) {
+	// определяем структуру теста
+	type want struct {
+		code        int
+		response    string
+		contentType string
+	}
+	// создаём массив тестов: имя и желаемый результат
+	tests := []struct {
+		name string
+		want want
+	}{
+		// определяем все тесты
+		{
+			name: "positive test #1",
+			want: want{
+				code:        400,
+				response:    `Sorry, something wrong. Try later. #400`,
+				contentType: "plain/text",
+			},
+		},
+	}
+	for _, tt := range tests {
+		// запускаем каждый тест
+		t.Run(tt.name, func(t *testing.T) {
+			request := httptest.NewRequest(http.MethodPost, "/update/gauge/testGauge/none", nil)
+
+			// создаём новый Recorder
+			w := httptest.NewRecorder()
+			// определяем хендлер
+			h := http.HandlerFunc(handlers.SaveMetric)
+			// запускаем сервер
+			h.ServeHTTP(w, request)
+			res := w.Result()
+
+			// проверяем код ответа
+			if res.StatusCode != tt.want.code {
+				t.Errorf("Expected status code %d, got %d", tt.want.code, w.Code)
+			}
+
+			// получаем и проверяем тело запроса
+			defer res.Body.Close()
+			resBody, err := io.ReadAll(res.Body)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if string(resBody) != tt.want.response {
+				t.Errorf("Expected body %s, got %s", tt.want.response, w.Body.String())
+			}
+
+			// заголовок ответа
+			if res.Header.Get("Content-Type") != tt.want.contentType {
+				t.Errorf("Expected Content-Type %s, got %s", tt.want.contentType, res.Header.Get("Content-Type"))
+			}
+		})
+	}
+}
+
+func TestSend404(t *testing.T) {
+	// определяем структуру теста
+	type want struct {
+		code        int
+		response    string
+		contentType string
+	}
+	// создаём массив тестов: имя и желаемый результат
+	tests := []struct {
+		name string
+		want want
+	}{
+		// определяем все тесты
+		{
+			name: "positive test #1",
+			want: want{
+				code:        404,
+				response:    `Sorry, something wrong. Try later. #404`,
+				contentType: "plain/text",
+			},
+		},
+	}
+	for _, tt := range tests {
+		// запускаем каждый тест
+		t.Run(tt.name, func(t *testing.T) {
+			request := httptest.NewRequest(http.MethodPost, "/update/counter/", nil)
+
+			// создаём новый Recorder
+			w := httptest.NewRecorder()
+			// определяем хендлер
+			h := http.HandlerFunc(handlers.SaveMetric)
+			// запускаем сервер
+			h.ServeHTTP(w, request)
+			res := w.Result()
+
+			// проверяем код ответа
+			if res.StatusCode != tt.want.code {
+				t.Errorf("Expected status code %d, got %d", tt.want.code, w.Code)
+			}
+
+			// получаем и проверяем тело запроса
+			defer res.Body.Close()
+			resBody, err := io.ReadAll(res.Body)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if string(resBody) != tt.want.response {
+				t.Errorf("Expected body %s, got %s", tt.want.response, w.Body.String())
+			}
+
+			// заголовок ответа
+			if res.Header.Get("Content-Type") != tt.want.contentType {
+				t.Errorf("Expected Content-Type %s, got %s", tt.want.contentType, res.Header.Get("Content-Type"))
+			}
+		})
+	}
+}
+
+func TestSend500(t *testing.T) {
+	// определяем структуру теста
+	type want struct {
+		code        int
+		response    string
+		contentType string
+	}
+	// создаём массив тестов: имя и желаемый результат
+	tests := []struct {
+		name string
+		want want
+	}{
+		// определяем все тесты
+		{
+			name: "positive test #1",
+			want: want{
+				code:        500,
+				response:    `Sorry, something wrong. Try later. #500`,
+				contentType: "plain/text",
+			},
+		},
+	}
+	for _, tt := range tests {
+		// запускаем каждый тест
+		t.Run(tt.name, func(t *testing.T) {
+			request := httptest.NewRequest(http.MethodPost, "/update/unknown/testCounter/100", nil)
+
+			// создаём новый Recorder
+			w := httptest.NewRecorder()
+			// определяем хендлер
+			h := http.HandlerFunc(handlers.SaveMetric)
 			// запускаем сервер
 			h.ServeHTTP(w, request)
 			res := w.Result()
