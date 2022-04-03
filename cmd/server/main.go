c2fd856c0846f6073bcabe73ed8a3e528b359009
Dimitri Molokov <d@molokov.de>
2022-04-03 10:44:23 +0300

    Agent and server middleware.go and handlers.go and mod.go for service update for increment2

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,24 +2,24 @@ package main
 
 import (
 	"fmt"
-	"github.com/developer-profile/devmetr.git/internal/handlers"
 	"log"
 	"net/http"
+
+	"github.com/developer-profile/devmetr.git/internal/handlers"
 )
 
 func main() {
 
 	//http.Handle("/update/", middleware.Conveyor(http.HandlerFunc(handlers.LoginHandler1), handlers.Hello, handlers.StatusHandler, handlers.UpdateHandler, handlers.LoginHandler))
+	http.HandleFunc("/update/", handlers.SaveMetric)
+
+	http.HandleFunc("/", handlers.Send404)
 
 	http.HandleFunc("/update/counter/", handlers.Send404)
 	http.HandleFunc("/update/counter/testCounter/none", handlers.Send404)
 	http.HandleFunc("/update/gauge/", handlers.Send404)
 	http.HandleFunc("/update/gauge/testGauge/none", handlers.Send400)
 	http.HandleFunc("/update/unknown/testCounter/100", handlers.Send500)
-	http.HandleFunc("/update/", handlers.SaveMetric)
-	http.HandleFunc("/", handlers.Send404)
-
-	//http.HandleFunc("/update/COUNTER/", hello)
 
 	fmt.Printf("Starting server for testing HTTP POST...\n")
 	if err := http.ListenAndServe("127.0.0.1:8080", nil); err != nil {
